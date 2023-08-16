671645185cacae679b4bc75a95ec987ec9d1c733
unknown <contatogabrielcruzrodrigues@gmail.com>
2023-08-16 09:08:29 -0300

    first commit

--- controller/validation.js
+++ controller/validation.js
@@ -1,15 +1,74 @@
 const express = require("express");
 const router = express.Router();
+const cookieParser = require("cookie");
 
 router.get("/", (req, res) => {
-    res.render("index");
-});
+    let emailErr, nomeErr, pontosErr;
+
+    nomeErr = req.flash("nomeErr");
+    emailErr = req.flash("emailErr");
+    pontosErr = req.flash("pontosErr");
+
+    nome = req.flash("nome");
+    email = req.flash("email");
+    pontos = req.flash("pontos");
+
+    nomeErr = (nomeErr == undefined || nomeErr.length == 0) ? undefined : nomeErr;
+    emailErr = (emailErr == undefined || emailErr.length == 0) ? undefined : emailErr;
+    pontosErr = (pontosErr == undefined || pontosErr.length == 0) ? undefined : pontosErr;
+
+    nome = (nome == undefined || nome.length == 0) ? undefined : nome;
+    email = (email == undefined || email.length == 0) ? undefined : email;
+    pontos = (pontos == undefined || pontos.length == 0) ? undefined : pontos;
+
+    res.render("index", {emailErr, nomeErr, pontosErr, nome: nome, email: email, pontos: pontos});
+}); 
 
 router.post("/form", (req, res) => {
-    let {email, nome, pontos} = req.body;
-    
-    console.log(email, nome, pontos);
-    res.send("oi");
+    let { email, nome, pontos } = req.body;
+    let emailErr, nomeErr, pontosErr;
+
+    if (!email) {
+        emailErr = "O email não pode ser nulo!";
+    };
+
+    if (!(email.includes("@gmail.com"))) {
+        emailErr = "digite um email valido";
+    };
+
+    if (!nome) {
+        nomeErr = "O nome não pode ser nulo";
+    };
+
+    if (nome.length < 4) {
+        nomeErr = "Digite um nome válido";
+    }
+
+    if (verification(nome)) {
+        nomeErr = "O seu nome não pode conter numeros ou caracteres";
+    }
+
+    function verification(text) {
+        return /[0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/.test(text);
+    };
+
+    if (pontos < 0 || pontos > 100) {
+        pontosErr = "os pontos devem estar entre 0 e 100";
+    };
+
+    if (emailErr != undefined || nomeErr != undefined || pontosErr != undefined) {
+        req.flash("emailErr", emailErr);
+        req.flash("nomeErr", nomeErr);
+        req.flash("pontosErr", pontosErr);
+
+        req.flash("email", email);
+        req.flash("nome", nome);
+        req.flash("pontos", pontos);
+
+        res.redirect("/");
+    } else {
+        res.send("form sent");
+    };
 })
 
 module.exports = router;
--- index.js
+++ index.js
@@ -3,17 +3,19 @@ const app = express();
 const session = require("express-session");
 const flash = require("express-flash");
 const validation = require('./controller/validation');
+const cookieParser = require("cookie-parser");
 const port = 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(cookieParser("açlskdjfgh"))
 
 app.use(session({
     secret: 'keyboard cat',
     resave: false,
     saveUninitialized: true,
-    cookie: {secure: true}
+    cookie: {maxAge: 60000}
 }));
 
 app.use(flash());
